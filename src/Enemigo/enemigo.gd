fc1bdee9de5b0725bb50dc56884e83d196dbb84e
Fran <franmiceli08@gmail.com>
2026-05-28 01:49:51 -0300

    adaptacion camara con PhantomCamera. hay que cambiar la logica de la escena principal y los niveles

--- src/Enemigo/enemigo.gd
+++ src/Enemigo/enemigo.gd
@@ -38,6 +38,6 @@ func _physics_process(delta: float) -> void:
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		if collision.get_collider().is_in_group("player"):
-			get_parent().perder()
+ww			get_parent().perder()
 		else: if collision.get_collider().is_in_group("obstaculo") and collision.get_collider().has_method("destruir"): 
 			collision.get_collider().destruir()
