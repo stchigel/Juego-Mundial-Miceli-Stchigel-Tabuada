ea35397afa22d7695f666c93e2adc20a0c09bed6
stchigel <Alumno26.STCHIGEL.GRIMALDI.Nicolas@ipm.edu.ar>
2026-05-30 16:45:14 -0300

    Funcionalidad de destrucción de obstáculo terminada

--- src/Enemigo/enemigo.gd
+++ src/Enemigo/enemigo.gd
@@ -41,6 +41,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("matar jugador")
 		emit_signal("matarJugador")
-	if body.is_in_group("obstaculo"):
+	if body.get_parent().is_in_group("obstaculo"):
 		print("obstaculo destruido")
-		body.destruir()
+		body.get_parent().destruir()
