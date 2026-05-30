0fc4cde6ee9798c20a5cb68a728bf5101fca0508
stchigel <Alumno26.STCHIGEL.GRIMALDI.Nicolas@ipm.edu.ar>
2026-05-30 17:18:32 -0300

    obstaculo de caja

--- src/Obstaculos/Trampolin/trampolin.gd
+++ src/Obstaculos/Trampolin/trampolin.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Roto.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,7 +10,10 @@ func _process(delta: float) -> void:
 	pass
 
 func destruir():
-	queue_free()
+	$Bien.hide()
+	$Roto.show()
+	$StaticBody2D.queue_free()
+	$Area2D.queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.get("_alturaSaltoTrampolin"):
