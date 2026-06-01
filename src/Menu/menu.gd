f39521e1db9d5d9592602a8bb280d4c8d8e098ed
Fran <franmiceli08@gmail.com>
2026-05-31 23:53:37 -0300

    personalizacion del menu

--- src/Menu/menu.gd
+++ src/Menu/menu.gd
@@ -4,3 +4,7 @@ func _on_button_pressed() -> void:
 	# volvemos el nivel global a 1 cada vez que empezamos una partida nueva
 	GlobalGameManager.nivel_actual = 1 
 	get_tree().change_scene_to_file("res://src/Game/game.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
